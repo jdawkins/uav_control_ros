636ebd0fd54aa9196a83487d3b6797c6c0afdd18
Ubuntu 12.04 <dawkins@basestation.(none)>
2014-07-03 16:05:31 -0400

    Added Functionality to control code

--- src/uav_control_node.cpp
+++ src/uav_control_node.cpp
@@ -2,6 +2,7 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Vector3.h>
 #include <sensor_msgs/Joy.h>
+#include <tf/transform_listener.h>
 
 #define STABILIZE_MODE 0    //Sets roll pitch yawrate and thrust
 #define ALT_HOLD_MODE 1     //Sets roll pitch yawrate and climbrate
@@ -12,7 +13,7 @@
 #define MAX_YAW_RATE PI     //Pi radians/sec half turn in a second
 #define MAX_RP_ANGLE 20*PI/180       //degrees max lean angle converted to radians
 #define MAX_RP_RATE PI
-#define HOVER_THRUST 0.3;
+#define DEFAULT_HOVER_THRUST 0.5;
 
 
 
@@ -24,16 +25,59 @@ ros::Publisher cmd_pub;
 nav_msgs::Odometry uav_cmd;
 
 float LX,LY,LT,RX,RY,RT,DPX,DPY;
-bool BA,BB,BX,BY;
+bool BA,BB,BX,BY,BL1,BR1;
 int flight_mode;
-std::string uav_type;
-nav_msgs::Odometry pose;
-double Kp,Kd,Kpsi;
-bool auto_control;
+std::string uav_type, uav_name,vicon_sub_msg;
+//nav_msgs::Odometry pose;
+geometry_msgs::TransformStamped  pose;
+double Kp,Kd,Kpsi,Khov,Kidle,thrust_range;
+bool armed;
 
+float roll_cmd;
+float pitch_cmd;
+float yawrate_cmd;
+
+
+void saturateCommands(nav_msgs::Odometry &cmd){
+
+//Saturate Thrust Commands
+    if(cmd.twist.twist.linear.z > 1){
+       cmd.twist.twist.linear.z = 1;
+    }
+    if(cmd.twist.twist.linear.z < 0){
+       cmd.twist.twist.linear.z = 0;
+    }
+
+//Saturate Roll and Pitch Commands
+    if(cmd.pose.pose.orientation.x > MAX_RP_ANGLE){
+        cmd.pose.pose.orientation.x = MAX_RP_ANGLE;
+    }else if(cmd.pose.pose.orientation.x < -MAX_RP_ANGLE){
+        cmd.pose.pose.orientation.x = -MAX_RP_ANGLE;
+    }else{
+
+    }
+
+    if(cmd.pose.pose.orientation.y > MAX_RP_ANGLE){
+        cmd.pose.pose.orientation.y = MAX_RP_ANGLE;
+    }else if(cmd.pose.pose.orientation.y < -MAX_RP_ANGLE){
+        cmd.pose.pose.orientation.y = -MAX_RP_ANGLE;
+    }else{
+
+    }
+
+////Saturate YawRate Commands
+    if(cmd.twist.twist.angular.z > MAX_YAW_RATE){
+        cmd.twist.twist.angular.z = MAX_YAW_RATE;
+    }else if(cmd.twist.twist.angular.z < -MAX_YAW_RATE){
+        cmd.twist.twist.angular.z = -MAX_YAW_RATE;
+    }else{
+
+    }
+}
 
 void joyCallBack(const sensor_msgs::Joy &joy_msg){
-    auto_control = false;
+    //auto_control = false;
+    //alt_control = false;
 
     LX = joy_msg.axes[0];
     LY = joy_msg.axes[1];
@@ -44,89 +88,121 @@ void joyCallBack(const sensor_msgs::Joy &joy_msg){
     BB = joy_msg.buttons[1];
     BX = joy_msg.buttons[2];
     BY = joy_msg.buttons[3];
+    BL1 = joy_msg.buttons[4];
+    BR1 = joy_msg.buttons[5];
 
-    if(BA){flight_mode = STABILIZE_MODE;}
-    if(BB){flight_mode = ALT_HOLD_MODE;}
-    if(BX){auto_control = true;}
-    if(BY){flight_mode = ACRO_MODE;}
-
-    switch(flight_mode){
-
-        case STABILIZE_MODE:
-        {
-            if(LY>0){ //only use positive values
-             uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
-
-            }else{
-             uav_cmd.twist.twist.linear.z = 0; //set to zero just to be sure
-            }
-
-            uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
-            uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
-            uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
-            break;
-
-        }
-        case ALT_HOLD_MODE:
-        {
-            uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
-            uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
-            uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
-            uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
-            break;
-        }
-        case AUTO_MODE:
-        {
-            uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
-            uav_cmd.pose.pose.orientation.z = LX;
-            uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
-            uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
-            break;
-        }
-        case ACRO_MODE:
-        {
-
-            uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
-            uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
-            uav_cmd.twist.twist.angular.x = MAX_RP_RATE*RX;
-            uav_cmd.twist.twist.angular.y = MAX_RP_RATE*RY;
-            break;
-        }
-        default:
-        break;
+    if(BA){flight_mode = STABILIZE_MODE;
+        armed = !armed;
     }
+    if(BB){flight_mode = STABILIZE_MODE;}
+    if(BX){flight_mode = STABILIZE_MODE;}
+    if(BY){flight_mode = STABILIZE_MODE;}
 
-}
 
-void poseCallBack(const nav_msgs::Odometry &pose_msg){
-    pose = pose_msg;
 
+    if(armed){
+
+        if(BL1){flight_mode = ALT_HOLD_MODE;
+            ROS_INFO("Alt Hold Mode");
+        }
+        if(BR1){flight_mode = AUTO_MODE;
+            ROS_INFO("Auto Mode Set");
+        }
+
+        switch(flight_mode){
+
+            case STABILIZE_MODE:
+            {
+                if(LY>0){ //only use positive values
+                 uav_cmd.twist.twist.linear.z = LY*(1 - Kidle) + Kidle; //Thust, use z velocity
+
+                }else{
+                 uav_cmd.twist.twist.linear.z = Kidle; //set to zero just to be sure
+                }
+
+                uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
+                uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
+                uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
+                break;
+
+            }
+            case ALT_HOLD_MODE:
+            {
+                //uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
+                uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
+                uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
+                uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
+                break;
+            }
+            case ACRO_MODE:
+            {
+
+                uav_cmd.twist.twist.linear.z = LY; //Thust, use z velocity
+                uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
+                uav_cmd.twist.twist.angular.x = MAX_RP_RATE*RX;
+                uav_cmd.twist.twist.angular.y = MAX_RP_RATE*RY;
+                break;
+            }
+            default:
+            break;
+        }
+    }else{
+        uav_cmd.twist.twist.linear.z = 0;
+        uav_cmd.twist.twist.angular.z = 0;
+        uav_cmd.pose.pose.orientation.x = 0;
+        uav_cmd.pose.pose.orientation.y = 0;
+
+    }
 }
 
 void accelCallBack(const geometry_msgs::Vector3 &des_accel){
 
+    switch(flight_mode){
+
+
+        case ALT_HOLD_MODE:
+        {
+            uav_cmd.twist.twist.linear.z = des_accel.z+Khov; //Thust, use z velocity
+            saturateCommands(uav_cmd);
+            //uav_cmd.twist.twist.angular.z = MAX_YAW_RATE*LX;
+            //uav_cmd.pose.pose.orientation.x = MAX_RP_ANGLE*RX;
+            //uav_cmd.pose.pose.orientation.y = MAX_RP_ANGLE*RY;
+            break;
+        }
+        case AUTO_MODE:
+        {
+
+        uav_cmd.pose.pose.orientation.x = -des_accel.x*sin(pose.transform.rotation.z) + des_accel.y*cos(pose.transform.rotation.z);
+        uav_cmd.pose.pose.orientation.y = -des_accel.x*cos(pose.transform.rotation.z) + des_accel.y*sin(pose.transform.rotation.z);
+
+        //uav_cmd.pose.pose.orientation.y = des_accel.x*cos(pose.transform.rotation.z) + des_accel.y*sin(pose.transform.rotation.z);
+        //uav_cmd.pose.pose.orientation.x = -des_accel.x*sin(pose.transform.rotation.z) + des_accel.y*cos(pose.transform.rotation.z);
+        uav_cmd.twist.twist.angular.z = Kpsi*(atan2(des_accel.x,des_accel.y) - pose.transform.rotation.z);
+        uav_cmd.twist.twist.linear.z = des_accel.z+Khov;
+        saturateCommands(uav_cmd);
+            break;
+        }
+
+        default:
+        break;
+    }
+
     //RollVeldes  = (1/g)*( Xcom(1,1)*sin(Ydes(3,1))- Xcom(2,1)*cos(Ydes(3,1)) );
     //PitchVeldes = (1/g)*( Xcom(1,1)*cos(Ydes(3,1))- Xcom(2,1)*sin(Ydes(3,1)) );
     //u1 = m*( g+Xaccel(3,1)+Kdz(Xvel(3,1)-zvel)+Kpz(Xpos(3,1)-zpos) );
-    if(auto_control){
-    uav_cmd.pose.pose.orientation.x = des_accel.x*cos(pose.pose.pose.orientation.z) - des_accel.y*sin(pose.pose.pose.orientation.z);
-    uav_cmd.pose.pose.orientation.y = des_accel.x*sin(pose.pose.pose.orientation.z) - des_accel.y*cos(pose.pose.pose.orientation.z);
-    uav_cmd.twist.twist.angular.z = atan2(des_accel.y,des_accel.x) - pose.pose.pose.orientation.z;
-    uav_cmd.twist.twist.linear.z = (0.2*des_accel.z)+0.5;
-    }
 
 }
 int main(int argc, char **argv){
+
 
     flight_mode = STABILIZE_MODE;
     ros::init(argc, argv, "uav_control_node");
 
     ros::NodeHandle nh;
-    auto_control = false;
+    armed = false;
 
     cmd_pub = nh.advertise<nav_msgs::Odometry>("uav_cmd",100);
     joy_sub = nh.subscribe("joy",1000,joyCallBack);
-    pose_sub = nh.subscribe("uav_pose",1000,poseCallBack);
     acc_sub = nh.subscribe("des_accel",1000,accelCallBack);
 
 
@@ -137,18 +213,28 @@ int main(int argc, char **argv){
         ROS_WARN("Defaulting to Uav type %s",uav_type.c_str());
     }
 
-    if(nh.getParam("gains/Kp",Kp)){
-        ROS_INFO("Using parameter Kp = %2.3f",Kp);
+    if(nh.getParam("uav_name",uav_name)){
+        ROS_INFO("Using parameter %s",uav_name.c_str());
     }else{
-        Kp = 0.5;
-        ROS_WARN("Defaulting to Kp Gain %2.3f",Kp);
+        uav_name = "generic_uav";
+        ROS_ERROR("You must define a UAV name which TF will be published on");
     }
-    if(nh.getParam("gains/Kd",Kd)){
-        ROS_INFO("Using parameter Kp = %2.3f",Kd);
+
+
+    if(nh.getParam("gains/Khov",Khov)){
+        ROS_INFO("Using parameter hover_thrust = %2.3f",Khov);
     }else{
-        Kd = 0.1;
-        ROS_WARN("Defaulting to Kd Gain %2.3f",Kd);
+        Khov = DEFAULT_HOVER_THRUST;
+        ROS_WARN("Defaulting to hover thrust %2.3f",Khov);
     }
+
+    if(nh.getParam("gains/Kidle",Kidle)){
+        ROS_INFO("Using parameter hover_thrust = %2.3f",Kidle);
+    }else{
+        Kidle = 0.1;
+        ROS_WARN("Defaulting to hover thrust %2.3f",Kidle);
+    }
+
 
     if(nh.getParam("gains/Kpsi",Kpsi)){
         ROS_INFO("Using parameter Kpsi = %2.3f",Kpsi);
@@ -157,8 +243,31 @@ int main(int argc, char **argv){
         ROS_WARN("Defaulting to Kpsi Gain %2.3f",Kpsi);
     }
 
+    vicon_sub_msg = "vicon/";
+    vicon_sub_msg.append(uav_name.c_str());
+    vicon_sub_msg.append("/Body");
+
+    thrust_range = 1 - Kidle;
+
+    tf::TransformListener listener;
+
     ros::Rate loop_rate(100);
     while(ros::ok()){
+
+
+        tf::StampedTransform transform;
+        try{
+          listener.lookupTransform("world",vicon_sub_msg,
+                                   ros::Time(0), transform);
+        }
+        catch (tf::TransformException ex){
+          ROS_ERROR("%s",ex.what());
+        }
+
+        tf::Quaternion q = transform.getRotation();
+
+        tf::Matrix3x3(q).getRPY(pose.transform.rotation.x,pose.transform.rotation.y,pose.transform.rotation.z);
+
 
         cmd_pub.publish(uav_cmd);
         ros::spinOnce();// Allow ROS to check for new ROS Messages
